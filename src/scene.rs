3f53639c75ea8adda59c5d7bf5b42a4e5d248093
Victor Aremu <me@victorare.mu>
2026-02-28 06:12:11 +0000

    fix(scene): remove duplicate derives

--- src/scene.rs
+++ src/scene.rs
@@ -495,8 +495,6 @@ pub(crate) enum PrimitiveBatch<'a> {
 
 #[derive(Default, Debug, Clone)]
 #[repr(C)]
-#[derive(Default, Debug, Clone)]
-#[repr(C)]
 pub(crate) struct Quad {
     pub order: DrawOrder,
     pub border_style: BorderStyle,
